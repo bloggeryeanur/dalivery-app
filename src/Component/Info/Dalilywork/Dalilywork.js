e045384823fce67e38f2decb04f0720b94dc2562
JsYeanur <jsyeanur@gmail.com>
2022-10-09 03:19:11 +0600

    image added

--- src/Component/Info/Dalilywork/Dalilywork.js
+++ src/Component/Info/Dalilywork/Dalilywork.js
@@ -3,12 +3,11 @@ import React from 'react'
 const Dalilywork = (props) => {
 
     const { btn } = props
-    const { name, jam, time, date } = props.info
+    const { name, jam, time, date,img } = props.info
     return (
         <div >
             <h3>{name}</h3>
-            <img style={{ height: '150px', width: '150px' }} src="https://i.ibb.co/NnGQVW3/pexels-alexander-cruzado-abanto-3686083.jpg
-https://i.ibb.co/ss4DZHm/pexels-mart-production-7706593.jpg"
+            <img style={{ height: '150px', width: '150px' }} src={img}
                 alt="" />
             <p>Zone : {jam}</p>
             <p>Date : {date}</p>
